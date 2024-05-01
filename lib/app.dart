b86e7bf8b8bcdf3c743ba9d819cf7bfbb10d0892
utso1707097 <utso097.csekuet@gmail.com>
2024-05-01 18:59:40 +0600

    Feat: onboarding screen done

--- lib/app.dart
+++ lib/app.dart
@@ -1,15 +1,18 @@
+import 'package:e_store/features/authentication/screens/onboarding/onboarding.dart';
 import 'package:e_store/utils/theme/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       themeMode: ThemeMode.system,
       theme: TAppTheme.lightTheme,
       darkTheme: TAppTheme.darkTheme,
+      home: const OnBoardingScreen(),
     );
   }
 }
---
+++ lib/features/authentication/screens/onboarding/onboarding.dart
@@ -0,0 +1,59 @@
+import 'package:e_store/features/authentication/controllers/onboarding/onboarding_controller.dart';
+import 'package:e_store/features/authentication/screens/onboarding/widgets/on_boarding_skip.dart';
+import 'package:e_store/utils/constants/image_strings.dart';
+import 'package:e_store/utils/constants/text_strings.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+
+import 'widgets/on_boarding_dot_navigation.dart';
+import 'widgets/on_boarding_next_button.dart';
+import 'widgets/onboarding_page.dart';
+
+class OnBoardingScreen extends StatelessWidget {
+  const OnBoardingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final controller = Get.put(OnBoardingController());
+    return Scaffold(
+      body: Stack(
+        children: [
+          /// Horizontal Scrollable Pages
+          PageView(
+            controller: controller.pageController,
+            onPageChanged: controller.updatePageIndicator,
+            children: const [
+              OnBoardingPage(
+                image: TImages.onBoardingImage1,
+                title: TTexts.onBoardingTitle1,
+                subTitle: TTexts.onBoardingSubTitle1,
+              ),
+              OnBoardingPage(
+                image: TImages.onBoardingImage2,
+                title: TTexts.onBoardingTitle2,
+                subTitle: TTexts.onBoardingSubTitle2,
+              ),
+              OnBoardingPage(
+                image: TImages.onBoardingImage3,
+                title: TTexts.onBoardingTitle3,
+                subTitle: TTexts.onBoardingSubTitle3,
+              ),
+            ],
+          ),
+
+          /// Skip Button
+          const OnBoardingSkip(),
+
+          /// Dot Navigation
+          const OnBoardingDotNavigation(),
+
+          /// Circular Button
+          const OnBoardingNextButton(),
+        ],
+      ),
+    );
+  }
+}
+
+
